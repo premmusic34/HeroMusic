e2dbc724ab76b3932079807714070926072081df
Shailendra34 <87485422+Shailendra34@users.noreply.github.com>
2021-11-27 21:13:52 +0530

    Add files via upload

---
+++ plugins/sysinfo.py
@@ -0,0 +1,50 @@
+import re
+import uuid
+import socket
+
+import psutil
+import platform
+from config import BOT_USERNAME
+from modules.helpers.command import commandpro
+from pyrogram import Client, filters
+from modules.helpers.decorators import humanbytes
+
+
+# FETCH SYSINFO
+
+@Client.on_message(commandpro(["Sysinfo", "/sysinfo", "sysinfo@{BOT_USERNAME}", "Shailendra", "Bot", "Admin"]) & ~filters.edited)
+async def give_sysinfo(client, message):
+    splatform = platform.system()
+    platform_release = platform.release()
+    platform_version = platform.version()
+    architecture = platform.machine()
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(socket.gethostname())
+    mac_address = ":".join(re.findall("..", "%012x" % uuid.getnode()))
+    processor = platform.processor()
+    ram = humanbytes(round(psutil.virtual_memory().total))
+    cpu_freq = psutil.cpu_freq().current
+    if cpu_freq >= 1000:
+        cpu_freq = f"{round(cpu_freq / 1000, 2)}GHz"
+    else:
+        cpu_freq = f"{round(cpu_freq, 2)}MHz"
+    du = psutil.disk_usage(client.workdir)
+    psutil.disk_io_counters()
+    disk = f"{humanbytes(du.used)} / {humanbytes(du.total)} " f"({du.percent}%)"
+    cpu_len = len(psutil.Process().cpu_affinity())
+    somsg = f"""🤖 **sʏsᴛᴇᴍ ɪɴғᴏʀᴍᴀᴛɪᴏɴ** 🤖
+    
+**ᴘʟᴀᴛғᴏʀᴍ :** `{splatform}`
+**ᴘʟᴀᴛғᴏʀᴍ - ʀᴇʟᴇᴀsᴇ :** `{platform_release}`
+**ᴘʟᴀᴛғᴏʀᴍ - ᴠᴇʀsɪᴏɴ :** `{platform_version}`
+**ᴀʀᴄʜɪᴛᴇᴄᴛᴜʀᴇ :** `{architecture}`
+**ʜᴏsᴛɴᴀᴍᴇ :** `{hostname}`
+**ɪᴘ :** `{ip_address}`
+**ᴍᴀᴄ :** `{mac_address}`
+**ᴘʀᴏᴄᴇssᴏʀ :** `{processor}`
+**ʀᴀᴍ : ** `{ram}`
+**ᴄᴘᴜ :** `{cpu_len}`
+**ᴄᴘᴜ ғʀᴇǫ :** `{cpu_freq}`
+**ᴅɪsᴋ :** `{disk}`
+    """
+    await message.reply(somsg)
