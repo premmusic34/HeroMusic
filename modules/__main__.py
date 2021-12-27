73df7efa146b5c93f1b270f2213b4b668ba686e2
Shailendra34 <87485422+Shailendra34@users.noreply.github.com>
2021-12-27 16:07:39 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -1,17 +1,14 @@
 import asyncio
 import requests
 from pytgcalls import idle
-from pytgcalls import PyTgCalls
-from pyrogram import Client as Bot
-from modules.config import arq, API_ID, API_HASH, BOT_TOKEN, BG_IMAGE, bot
+from modules.config import arq, BG_IMAGE, call_py
 
 response = requests.get(BG_IMAGE)
 with open("./resource/thumbnail.png", "wb") as file:
     file.write(response.content)
     
 
-call_py = PyTgCalls(bot)
-                    
+
                     
 
 async def main():
