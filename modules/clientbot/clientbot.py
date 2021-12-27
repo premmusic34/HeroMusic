590829233a9f81734a3afe36a78ba353e6c18df8
Shailendra34 <87485422+Shailendra34@users.noreply.github.com>
2021-12-27 21:33:17 +0530

    Update clientbot.py

--- modules/clientbot/clientbot.py
+++ modules/clientbot/clientbot.py
@@ -1,4 +1,3 @@
-from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import InputStream
@@ -6,12 +5,9 @@ from pytgcalls.types.input_stream import InputAudioStream
 from modules import config
 from . import queues
 
-client = Client(config.STRING_SESSION, config.API_ID, config.API_HASH)
-pytgcalls = PyTgCalls(client)
-
 
 @pytgcalls.on_stream_end()
-async def on_stream_end(client: PyTgCalls, update: Update) -> None:
+async def on_stream_end(bot: PyTgCalls, update: Update) -> None:
     chat_id = update.chat_id
     queues.task_done(chat_id)
 
@@ -27,4 +23,4 @@ async def on_stream_end(client: PyTgCalls, update: Update) -> None:
             ),
         )
       
-run = pytgcalls.start
+
